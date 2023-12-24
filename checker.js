dbb5cd955a91feda46cd4a10aab73a78a9ebda77
David Pinkerton <david@dpinkerton.com>
2023-12-24 19:53:58 +1000

    Partial implementation of pub key calculation. Browserify needed.

--- checker.js
+++ checker.js
@@ -11,3 +11,25 @@ function loadConfiguration() {
     // Add a new Option element for each xpub key found
     xpubs.forEach(xpub => select.add(new Option(xpub)));
 }
+
+const bip32 = require('bip32');
+
+function getPublicKeyFromDerivationPath(xpub, path) {
+    const publicKey = bip32.fromBase58(xpub).derivePath(path).publicKey;
+    return publicKey;
+  }
+
+
+function calculatePublicKey() {
+  // Get the selected xpub key from the dropdown list
+  const xpub = document.getElementById("xpubs").value;
+
+  // Get the derivation path from the input field
+  const path = document.getElementById("path").value;
+
+  // Call the getPublicKeyFromDerivationPath function to obtain the public key
+  const publicKey = getPublicKeyFromDerivationPath(xpub, path);
+
+  // Display the public key on the page
+  document.getElementById("public-key").innerHTML = publicKey;
+}
