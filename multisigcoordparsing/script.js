d726577c3ef6f2f0fb4d3c751c98cd0b0cfde13a
David Pinkerton <david@dpinkerton.com>
2023-12-30 21:42:19 +1000

    Detect paths with h instead of apostrophe

--- multisigcoordparsing/script.js
+++ multisigcoordparsing/script.js
@@ -1,72 +1,23 @@
-function extractPaths() {
-    // Get the input text
-    const inputText = document.getElementById('inputText').value.trim();
+function extractPathsAndXpubsFromMultisigConfig(multisigConfig) {
+    const pathsRegex = /\/[\dh'\/]+(?:[h'](?=\d)|[h'])/g;
+    const xpubsRegex = /\b\w*xpub\w*\b/g;
 
-    // Check if inputText is empty
-    if (!inputText) {
-        alert('Please enter some text.');
-        return;
-    }
+    const extractMatches = (regex) => [...multisigConfig.matchAll(regex)].map(match => match[0]);
 
-    // Clear previous results
-    const addressList = document.getElementById('addressList');
-    addressList.innerHTML = '';
+    const associatedPathsAndXpubs = () => {
+        const xpubs = extractMatches(xpubsRegex);
+        const parts = multisigConfig.split(/\b\w*xpub\w*\b/);
 
-    // Extract Bitcoin address derivation paths and xpubs from the input text
-    const { xpubs, pathsDictionary } = extractXpubsAndPaths(inputText);
+        return xpubs.map((xpub, index) => ({
+            path: (parts[index].match(pathsRegex) || ['unknown'])[0],
+            xpub,
+        }));
+    };
 
-    // Display tidied paths in the list
-    xpubs.forEach(xpub => {
-        const listItem = document.createElement('li');
-        const paths = pathsDictionary[xpub] || 'not apparent';
-        listItem.textContent = `${xpub}: ${paths}`;
-        addressList.appendChild(listItem);
-    });
+    return associatedPathsAndXpubs();
 }
 
-function extractXpubsAndPaths(inputText) {
-    const xpubs = [];
-    const pathsDictionary = {};
-
-    // Define a regular expression to match xpubs
-    const xpubRegex = /\b\w*xpub\w*\b/g;
-
-    // Find all matches in the input text
-    const xpubMatches = inputText.match(xpubRegex) || [];
-
-    // Iterate over xpub matches
-    xpubMatches.forEach(xpub => {
-        // Find the index of the current xpub in the input text
-        const xpubIndex = inputText.indexOf(xpub);
-
-        // Extract the substring from the beginning to the current xpub
-        const substringBeforeXpub = inputText.substring(0, xpubIndex);
-
-        // Extract derivation paths from the substring before the xpub
-        const paths = extractDerivationPaths(substringBeforeXpub);
-
-        // Store xpub and associated paths in the dictionary
-        xpubs.push(xpub);
-        pathsDictionary[xpub] = paths;
-    });
-
-    return { xpubs, pathsDictionary };
-}
-
-// The rest of your code remains unchanged...
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Example usage:
+const multisigConfig = "BSMS 1.0 wsh(sortedmulti(2,[d2060b25 m/48'/0'/0'/2h ]xpub6EgnSzhMJxPcHJKTM8UW3yR5A4gQBtDfRMy89qV9U99hpCmr7EBSbn2py9W3b6ZQjznxocDThg7QogD1itv1SbWYUKkeYizbFB1H5isEoJu,[b58bd39f/48h/0h/0h/2h]xpub6EBVpdpMMhzyvZLQeDvS4ChXqeJHNRUsMRrRmHKSZKoqxiEiwaEYGuYRvqJGzuKXXXK5bmtPdcDdUxNgnKuLD4xMqEihPtDaedNijaUdvAo,[ddac3619/48h/0h/0h/2h]xpub6DzrNuhmcXd397JyaQ9yHzCENNtqYxDJtaU8zd3ok7Qz5vfAk1ZjpfT5W3qkHKGFX7PYt9sf7tS7yMBaMwyBBtBA9mB2quYcwR8nUqrpeaT))/0/*,/1/*bc1qgdn6r06nknx3gaqnd67ygn748v894dpw79qrggqp34y2j6dhqfyqghg7h8# Exported from Nunchuk Name: Noosa testing Policy: 1 of 3 Format: P2WSH Derivation: m/48'/0'/0'/2'0e589c39:xpub6ETbkTdCWxhFmizpXGQ8U7Lx7kKQ8CnnRNps96Q8Z4XX7JAAEvCHeJv5UM5kU1Cm3kJUgr5PeQkFqUrXAfetxD1skJ7jie7qMVm7BMdJe36Derivation: m/48'/0'/0'/2'46b5d9e5:xpub6FQywvvaYNWevawA3PkVMa7mmpP17zPkzKew18NZvQqjw9Q2ixyKwzoowVVgmmrHveDEwioLSRf6kvSEmjLqpgQY44pki8iKU6wXKeHBLKcDerivation: m/48'/0'/2'/2'1756fb36:xpub6EsDuBZNA3gQUFHh5WXoF1PNx7HEAyY9HEPAa5Qid4TRBXomAdqfL1XmEiLFbTc4hAVu7JWA5iQ5YKmbgvK1sSpmazSpqZPogz84ArEtSuU";
+const result = extractPathsAndXpubsFromMultisigConfig(multisigConfig);
+console.log(result);
